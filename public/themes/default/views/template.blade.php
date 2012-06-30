1407138a0917e20572c43798732bff9bd478272c
Jason Lewis <jason.lewis1991@gmail.com>
2012-06-30 17:17:08 +0930

    Working on the category/discussion view.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- public/themes/default/views/template.blade.php
+++ public/themes/default/views/template.blade.php
@@ -40,129 +40,95 @@
 						
 						<ul class="discussions">
 
-							<li class="heading">
+							<li class="category">
 								<h2>Release Announcements</h2>
 
-								<div class="child-category">
-
-								</div>
+								<ul class="children">
+									<li><a href="">3.x Announcements</a></li>
+									<li><a href="">2.x Announcements</a></li>
+									<li><a href="">1.x Announcements</a></li>
+								</ul>
 							</li>
 
 							<li class="discussion group">
-								<div class="channel">
-									<a href="">Release Announcements</a>
+								<div class="category">
+									<a href="" class="btn soft">2.x Announcements</a>
 								</div>
 
 								<div class="meta">
-									<span class="label label-inverse">Sticky</span>
-									<span class="label label-inverse">Closed</span>
-									
-									<a href="" class="title">No help zone</a>
+									<a href="" class="title">2.2 Released</a>
 									<p>
-										Started by <a href="">Jason</a>, <span title="24th June, 2012 at 6:45am">24th June</span>
+										Started by <a href="">Jason</a>, <span title="24th June, 2012 at 6:45am">22th June</span>
 									</p>
 
 									<p>
-										Last reply by <a href="">Dayle</a>, on <span title="24th June, 2012 at 6:45am">24th June</span>
+										Last reply by <a href="">Dayle</a>, on <span title="24th June, 2012 at 6:45am">23th June</span>
 									</p>
 								</div>
 
 								<div class="stats">
 									<div class="views">
 										<span class="title">Views</span>
-										<span class="number">2k</span>
+										<span class="number">1.2k</span>
 									</div>
 									<div class="replies">
 										<span class="title">Replies</span>
-										<span class="number">0</span>
+										<span class="number">38</span>
 									</div>
 								</div>
 							</li>
 
 							<li class="discussion group">
-								<div class="channel">
-									<a href="">Release Announcements</a>
+								<div class="category">
+									<a href="" class="btn soft">3.x Announcements</a>
 								</div>
 
 								<div class="meta">
-									<span class="label label-inverse">Sticky</span>
-									<span class="label label-inverse">Closed</span>
-									
-									<a href="" class="title">No help zone</a>
+									<a href="" class="title">3.0 Beta</a>
 									<p>
-										Started by <a href="">Jason</a>, <span title="24th June, 2012 at 6:45am">24th June</span>
+										Started by <a href="">Taylor</a>, <span title="24th June, 2012 at 6:45am">25th June</span>
 									</p>
 
 									<p>
-										Last reply by <a href="">Dayle</a>, on <span title="24th June, 2012 at 6:45am">24th June</span>
+										Last reply by <a href="">Phill</a>, on <span title="24th June, 2012 at 6:45am">27th June</span>
 									</p>
 								</div>
 
 								<div class="stats">
 									<div class="views">
 										<span class="title">Views</span>
-										<span class="number">2k</span>
+										<span class="number">941</span>
 									</div>
 									<div class="replies">
 										<span class="title">Replies</span>
-										<span class="number">0</span>
-									</div>
-								</div>
-							</li>
-
-							<li class="discussion group">
-								<div class="channel">
-									<a href="">Release Announcements</a>
-								</div>
-
-								<div class="meta">
-									<span class="label label-inverse">Sticky</span>
-									<span class="label label-inverse">Closed</span>
-									
-									<a href="" class="title">No help zone</a>
-									<p>
-										Started by <a href="">Jason</a>, <span title="24th June, 2012 at 6:45am">24th June</span>
-									</p>
-
-									<p>
-										Last reply by <a href="">Dayle</a>, on <span title="24th June, 2012 at 6:45am">24th June</span>
-									</p>
-								</div>
-
-								<div class="stats">
-									<div class="views">
-										<span class="title">Views</span>
-										<span class="number">2k</span>
-									</div>
-									<div class="replies">
-										<span class="title">Replies</span>
-										<span class="number">0</span>
+										<span class="number">123</span>
 									</div>
 								</div>
 							</li>
 
 							<li class="more">
-								See all Release Announcements
+								<a href="" class="btn soft">See 327 more discussions <span>&rarr;</span></a>
 							</li>
 
-							<li class="heading">
+							<li class="category">
 								<h2>General Discussions</h2>
 							</li>
 
 							<li class="discussion group">
-								<div class="channel">
-									<a href="">General Discussions</a>
+								<div class="category">
+									<a href="" class="btn soft">General Discussions</a>
 								</div>
 
 								<div class="meta">
+									<span class="label label-inverse">Sticky</span>
 									<a href="" class="title">How have you found Laravel?</a>
 									
 									<p>
-										Started by <a href="">Taylor</a>, <span title="24th June, 2012 at 6:45am">24th June</span>
+										Started by <a href="">Michael</a>, <span title="24th June, 2012 at 6:45am">24th June</span>
 									</p>
 
 									<p>
-										Last reply by <a href="">Phill</a>, on <span title="24th June, 2012 at 6:45am">24th June</span>
+										Last reply by <a href="">Dayle</a>, on <span title="24th June, 2012 at 6:45am">24th June</span>
 									</p>
 								</div>
 
@@ -176,6 +142,39 @@
 										<span class="number">460</span>
 									</div>
 								</div>
+							</li>
+
+							<li class="discussion group">
+								<div class="category">
+									<a href="" class="btn soft">General Discussions</a>
+								</div>
+
+								<div class="meta">
+									<a href="" class="title">The Future!</a>
+									
+									<p>
+										Started by <a href="">Taylor</a>, <span title="24th June, 2012 at 6:45am">30th June</span>
+									</p>
+
+									<p>
+										Last reply by <a href="">Shawn</a>, on <span title="24th June, 2012 at 6:45am">30th June</span>
+									</p>
+								</div>
+
+								<div class="stats">
+									<div class="views">
+										<span class="title">Views</span>
+										<span class="number">7.3k</span>
+									</div>
+									<div class="replies">
+										<span class="title">Replies</span>
+										<span class="number">752</span>
+									</div>
+								</div>
+							</li>
+
+							<li class="more">
+								<a href="" class="btn soft">See 1,214 more discussions <span>&rarr;</span></a>
 							</li>
 
 						</ul>
